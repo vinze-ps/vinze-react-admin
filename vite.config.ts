dc462f53949b76ae47fa40b791665a4b0a68b922
vinze-ps <patryk.surmacz12@gmail.com>
2024-09-22 23:15:32 +0200

    next

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,57 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
       insertTypesEntry: true,
-      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
     }),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),
-      name: 'VinzeReactAdmin',
-      formats: ['es', 'cjs'],
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      name: "VinzeReactAdmin",
+      formats: ["es", "cjs"],
       fileName: (format) => `vinze-react-admin.${format}.js`,
     },
     rollupOptions: {
       external: [
-        'react',
-        'react-dom',
-        '@nextui-org/react',
-        'lucide-react',
-        'moment',
+        "react",
+        "react-dom",
+        "@nextui-org/react",
+        "lucide-react",
+        "moment",
         "date-fns",
         "framer-motion",
+        "@untitled-ui/icons-react",
+        "@radix-ui/react-icons",
+        "@tiptap/core",
       ],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          '@nextui-org/react': 'NextUI',
-          'lucide-react': 'LucideReact',
-          moment: 'moment',
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@nextui-org/react": "NextUI",
+          "lucide-react": "LucideReact",
+          moment: "moment",
           "date-fns": "DateFns",
           "framer-motion": "FramerMotion",
+          "@untitled-ui/icons-react": "UntitledUIIcons",
+          "@radix-ui/react-icons": "RadixUIReactIcons",
+          "@tiptap/core": "TiptapCore",
         },
-        assetFileNames: 'assets/[name].[ext]',
+        assetFileNames: "assets/[name].[ext]",
       },
     },
-    cssCodeSplit: true
+    cssCodeSplit: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
